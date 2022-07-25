d3abee58fd45ada94eb93c316fb3a1bce2f2b829
URK21CS3011-Anisha <109951728+URK21CS3011-Anisha@users.noreply.github.com>
2022-07-25 16:35:53 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -19,8 +19,8 @@ class Stack:
             
 
     def status(self):
-        for i in range(self.top+1):
-            print(self.items[i])
+        for j in range(self.top+1):
+            print(self.items[j])
 
 
 # Do not change the following code
