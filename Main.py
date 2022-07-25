06be7afebaafe05f44d729e23ae3170b37fd54f4
URK21CS3011-Anisha <109951728+URK21CS3011-Anisha@users.noreply.github.com>
2022-07-25 16:15:24 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,9 +11,7 @@ class Stack:
         return self.top==self.size-1
     def push(self, data):
          if not self.is_full():
-            
             self.top+=1
-            
             self.items[self.top]=data
     def pop(self):
        if not self.is_empty():
